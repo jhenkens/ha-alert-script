98e9a62d3d98ef5535c84a6cba3c0222fe92d652
Johan Henkens <johan@henkens.com>
2025-10-21 15:37:34 -0700

    Add changes from upstream

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ha-alert-script
 Copied from Home Assistant's Alert integration, but added data-templating and script calling. Using this as an interim until blueprints are updated to better re-create alert, and alert is deprecated. See https://github.com/home-assistant/home-assistant.io/pull/28930
+
+## Features
+- All features from the original Home Assistant Alert integration
+- Data templating support for notifications
+- Script calling on alert events
+- **NEW**: `unique_id` support for better entity management
+
+## Configuration
+
+```yaml
+alert_script:
+  my_alert:
+    name: "My Alert"
+    entity_id: sensor.my_sensor
+    state: "on"
+    repeat: [5, 10, 30]  # minutes
+    unique_id: "my_unique_alert_id"  # Optional: Set a unique ID for this alert
+    notifiers:
+      - mobile_app_my_phone
+    message: "Alert: {{ states('sensor.my_sensor') }}"
+    data:
+      priority: high
+    script: my_alert_script
+    variables:
+      sensor_name: "{{ state_attr('sensor.my_sensor', 'friendly_name') }}"
+```
+
+The `unique_id` parameter is optional and allows you to:
+- Better manage entities in the Home Assistant UI
+- Ensure entity persistence across configuration changes
+- Enable proper entity registry management
--- custom_components/alert_script/__init__.py
+++ custom_components/alert_script/__init__.py
@@ -1,47 +1,25 @@
 """Support for repeating alerts when conditions are met."""
 from __future__ import annotations
 
-from collections.abc import Callable
-from datetime import timedelta
 from typing import Any
 
 import voluptuous as vol
 
-from homeassistant.components.notify import (
-    ATTR_DATA,
-    ATTR_MESSAGE,
-    ATTR_TITLE,
-    DOMAIN as DOMAIN_NOTIFY,
-)
-
-from homeassistant.components.script import (
-    DOMAIN as DOMAIN_SCRIPT,
-)
 from homeassistant.const import (
     CONF_ENTITY_ID,
     CONF_NAME,
     CONF_REPEAT,
     CONF_STATE,
+    CONF_UNIQUE_ID,
     SERVICE_TOGGLE,
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
-    STATE_IDLE,
-    STATE_OFF,
     STATE_ON,
 )
-from homeassistant.core import HassJob, HomeAssistant
-from homeassistant.exceptions import ServiceNotFound
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_component import EntityComponent
-from homeassistant.helpers.event import (
-    EventStateChangedData,
-    async_track_point_in_time,
-    async_track_state_change_event,
-)
-from homeassistant.helpers.template import Template
-from homeassistant.helpers.typing import ConfigType, EventType
-from homeassistant.util.dt import now
+from homeassistant.helpers.typing import ConfigType
 
 from .const import (
     CONF_ALERT_MESSAGE,
@@ -59,7 +37,8 @@ from .const import (
     LOGGER,
 )
 
-ALERT_SCHEMA = vol.Schema(
+from .entity import AlertScriptEntity
+ALERT_SCRIPT_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_NAME): cv.string,
         vol.Required(CONF_ENTITY_ID): cv.entity_id,
@@ -85,19 +64,20 @@ ALERT_SCHEMA = vol.Schema(
                 cv.template, vol.All(dict, cv.template_complex)
             ),
         vol.Optional(CONF_SCRIPT): cv.string,
+        vol.Optional(CONF_UNIQUE_ID): cv.string,
     }
 )
 
 CONFIG_SCHEMA = vol.Schema(
-    {DOMAIN: cv.schema_with_slug_keys(ALERT_SCHEMA)}, extra=vol.ALLOW_EXTRA
+    {DOMAIN: cv.schema_with_slug_keys(ALERT_SCRIPT_SCHEMA)}, extra=vol.ALLOW_EXTRA
 )
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Alert component."""
-    component = EntityComponent[AlertScript](LOGGER, DOMAIN, hass)
+    component = EntityComponent[AlertScriptEntity](LOGGER, DOMAIN, hass)
 
-    entities: list[AlertScript] = []
+    entities: list[AlertScriptEntity] = []
 
     for object_id, cfg in config[DOMAIN].items():
         if not cfg:
@@ -116,9 +96,10 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         data: dict[str, Any] | None = cfg.get(CONF_DATA)
         variables: dict[str, Any] | None = cfg.get(CONF_VARIABLES)
         script: str | None | None = cfg.get(CONF_SCRIPT)
+        unique_id: str | None = cfg.get(CONF_UNIQUE_ID)
 
         entities.append(
-            AlertScript(
+            AlertScriptEntity(
                 hass,
                 object_id,
                 name,
@@ -133,251 +114,18 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
                 title_template,
                 data,
                 variables,
-                script
+                script,
+                unique_id
             )
         )
 
     if not entities:
         return False
 
-    component.async_register_entity_service(SERVICE_TURN_OFF, {}, "async_turn_off")
-    component.async_register_entity_service(SERVICE_TURN_ON, {}, "async_turn_on")
-    component.async_register_entity_service(SERVICE_TOGGLE, {}, "async_toggle")
+    component.async_register_entity_service(SERVICE_TURN_OFF, None, "async_turn_off")
+    component.async_register_entity_service(SERVICE_TURN_ON, None, "async_turn_on")
+    component.async_register_entity_service(SERVICE_TOGGLE, None, "async_toggle")
 
     await component.async_add_entities(entities)
 
     return True
-
-
-class AlertScript(Entity):
-    """Representation of an alert."""
-
-    _attr_should_poll = False
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        entity_id: str,
-        name: str,
-        watched_entity_id: str,
-        state: str,
-        repeat: list[float],
-        skip_first: bool,
-        message_template: Template | None,
-        done_message_template: Template | None,
-        notifiers: list[str],
-        can_ack: bool,
-        title_template: Template | None,
-        data: dict[str, Any] | None,
-        variables: dict[str,Any] | None,
-        script: str | None,
-    ) -> None:
-        """Initialize the alert."""
-        self.hass = hass
-        self._attr_name = name
-        self._alert_state = state
-        self._skip_first = skip_first
-        self._data = data
-        self._variables = variables
-
-        self._message_template = message_template
-        if self._message_template is not None:
-            self._message_template.hass = hass
-
-        self._done_message_template = done_message_template
-        if self._done_message_template is not None:
-            self._done_message_template.hass = hass
-
-        self._title_template = title_template
-        if self._title_template is not None:
-            self._title_template.hass = hass
-
-        self._notifiers = notifiers
-        self._script = script
-        self._can_ack = can_ack
-
-        self._delay = [timedelta(minutes=val) for val in repeat]
-        self._next_delay = 0
-
-        self._firing = False
-        self._ack = False
-        self._cancel: Callable[[], None] | None = None
-        self._send_done_message = False
-        self.entity_id = f"{DOMAIN}.{entity_id}"
-        
-
-        async_track_state_change_event(
-            hass, [watched_entity_id], self.watched_entity_change
-        )
-
-    @property
-    def state(self) -> str:
-        """Return the alert status."""
-        if self._firing:
-            if self._ack:
-                return STATE_OFF
-            return STATE_ON
-        return STATE_IDLE
-
-    async def watched_entity_change(
-        self, event: EventType[EventStateChangedData]
-    ) -> None:
-        """Determine if the alert should start or stop."""
-        if (to_state := event.data["new_state"]) is None:
-            return
-        LOGGER.debug("Watched entity (%s) has changed", event.data["entity_id"])
-        if to_state.state == self._alert_state and not self._firing:
-            await self.begin_alerting()
-        if to_state.state != self._alert_state and self._firing:
-            await self.end_alerting()
-
-    async def begin_alerting(self) -> None:
-        """Begin the alert procedures."""
-        LOGGER.debug("Beginning Alert: %s", self._attr_name)
-        self._ack = False
-        self._firing = True
-        self._next_delay = 0
-
-        if not self._skip_first:
-            await self._notify()
-        else:
-            await self._schedule_notify()
-
-        self.async_write_ha_state()
-
-    async def end_alerting(self) -> None:
-        """End the alert procedures."""
-        LOGGER.debug("Ending Alert: %s", self._attr_name)
-        if self._cancel is not None:
-            self._cancel()
-            self._cancel = None
-
-        self._ack = False
-        self._firing = False
-        if self._send_done_message:
-            await self._notify_done_message()
-        self.async_write_ha_state()
-
-    async def _schedule_notify(self) -> None:
-        """Schedule a notification."""
-        delay = self._delay[self._next_delay]
-        next_msg = now() + delay
-        self._cancel = async_track_point_in_time(
-            self.hass,
-            HassJob(
-                self._notify, name="Schedule notify alert", cancel_on_shutdown=True
-            ),
-            next_msg,
-        )
-        self._next_delay = min(self._next_delay + 1, len(self._delay) - 1)
-    
-    def _data_template_creator(self, value: Any, **kwargs) -> Any:
-        """Recursive template creator helper function."""
-        if isinstance(value, list):
-            return [self._data_template_creator(item) for item in value]
-        if isinstance(value, dict):
-            return {
-                key: self._data_template_creator(item) for key, item in value.items()
-            }
-        if not isinstance(value, Template):
-            return value
-        value.hass = self.hass
-        return value.async_render(kwargs, parse_result=False)
-
-    async def _notify(self, *args: Any) -> None:
-        """Send the alert notification."""
-        if not self._firing:
-            return
-
-        if not self._ack:
-            LOGGER.info("Alerting: %s", self._attr_name)
-            self._send_done_message = True
-
-            if self._message_template is not None:
-                message = self._message_template.async_render(parse_result=False)
-            else:
-                message = self._attr_name
-
-            await self._send_notification_message(message)
-            await self._call_script("fire")
-        await self._schedule_notify()
-
-    async def _notify_done_message(self) -> None:
-        """Send notification of complete alert."""
-        LOGGER.info("Alerting: %s", self._done_message_template)
-        self._send_done_message = False
-
-        if self._done_message_template is not None:
-            message = self._done_message_template.async_render(parse_result=False)
-            await self._send_notification_message(message)
-        await self._call_script("done")
-        
-    async def _call_script(self, event: str) -> None:
-        if not self._script:
-            return
-        
-        script_variables = {"event":event}
-
-        if self._variables:
-            script_variables.update(self._data_template_creator(self._variables))
-
-        LOGGER.debug(script_variables)
-
-        try:
-            await self.hass.services.async_call(
-                DOMAIN_SCRIPT, self._script, script_variables, context=self._context
-            )
-        except ServiceNotFound:
-            LOGGER.error(
-                "Failed to call script.%s, retrying at next notification interval",
-                self._script,
-            )
-
-    async def _send_notification_message(self, message: Any) -> None:
-        if not self._notifiers:
-            return
-
-        msg_payload = {ATTR_MESSAGE: message}
-
-        if self._title_template is not None:
-            title = self._title_template.async_render(parse_result=False)
-            msg_payload[ATTR_TITLE] = title
-        if self._data:
-            msg_payload[ATTR_DATA] = self._data_template_creator(self._data)
-
-        LOGGER.debug(msg_payload)
-
-        for target in self._notifiers:
-            try:
-                await self.hass.services.async_call(
-                    DOMAIN_NOTIFY, target, msg_payload, context=self._context
-                )
-            except ServiceNotFound:
-                LOGGER.error(
-                    "Failed to call notify.%s, retrying at next notification interval",
-                    target,
-                )
-
-    async def async_turn_on(self, **kwargs: Any) -> None:
-        """Async Unacknowledge alert."""
-        LOGGER.debug("Reset Alert: %s", self._attr_name)
-        self._ack = False
-        self.async_write_ha_state()
-        await self._call_script("unack")
-
-    async def async_turn_off(self, **kwargs: Any) -> None:
-        """Async Acknowledge alert."""
-        LOGGER.debug("Acknowledged Alert: %s", self._attr_name)
-        self._ack = True
-        self.async_write_ha_state()
-        await self._call_script("ack")
-
-    async def async_toggle(self, **kwargs: Any) -> None:
-        """Async toggle alert."""
-        if self._ack:
-            return await self.async_turn_on()
-        return await self.async_turn_off()
-
-
-# Todo: 
-# 1. Why is check_templates_warn angry at us? Can we figure that out and clean it up?
